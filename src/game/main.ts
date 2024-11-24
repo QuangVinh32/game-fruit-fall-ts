769e5adf766c05778764ae196d73fbe5b0a10ca6
QuangVinh32 <130158949+QuangVinh32@users.noreply.github.com>
2024-11-24 15:10:10 +0700

    Update main.ts
    comment

--- src/game/main.ts
+++ src/game/main.ts
@@ -25,7 +25,7 @@ const config: Phaser.Types.Core.GameConfig = {
         default: 'arcade',
         arcade: {
             gravity: { x: 0, y: 0 },
-            debug: true,
+            // debug: true,
             // debugShowVelocity: false
         }
     },
